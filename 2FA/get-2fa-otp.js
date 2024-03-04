acbf8ecbd084c88a6964e45c9301c39f5c050a30
lowji194 <theloi194@gmail.com>
2024-03-04 11:48:19 +0700

    Add files via upload

---
+++ 2FA/get-2fa-otp.js
@@ -0,0 +1,11 @@
+function getIndexHtmlUrl() {
+    // Lấy URL của trang web hiện tại
+    var currentUrl = window.location.href;
+
+    // Trả về URL của trang web hiện tại
+    return currentUrl;
+}
+
+// Sử dụng hàm để lấy URL của trang web (index.html)
+var indexHtmlUrl = getIndexHtmlUrl();
+       alert(indexHtmlUrl);
